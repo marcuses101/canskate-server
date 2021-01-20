a2ba0db6482a405fda78f97c5bc6fd698ffa8f24
marcuses101 <mnjconnolly@gmail.com>
2021-01-20 11:14:03 -0500

    remove unused files

--- elements-query.sql
+++
@@ -1,6 +0,0 @@
-SELECT e.description Element, c.id, r.stage as Badge, r.fundamental_area as Fundamental
-FROM canskate_elements e
-JOIN canskate_checkmarks c
-ON e.checkmark_id = c.id
-JOIN canskate_ribbons r
-ON c.ribbon_id = r.id;
